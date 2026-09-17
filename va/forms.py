a579784ca78bf2cd8572523a9ecc1f9f1ddb2794
Grace Kabwe <gracekbwe@gmail.com>
2026-09-17 11:18:20 +0200

    Add bootstrap styling to the form

--- va/forms.py
+++ va/forms.py
@@ -2,14 +2,23 @@ from django import forms
 from .models import VerbalAutopsy
 
 class VerbalAutopsyForm(forms.ModelForm):
+    
+    #this adds form-control to every field
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+        
     class Meta:
         model = VerbalAutopsy
         fields = "__all__"
+        
         widgets = {
             "date_of_death": forms.DateInput(
-                attrs={"type": "date"}
+                attrs={"type": "date", "class": "form-control"}
             ),
             "interview_date": forms.DateInput(
-                attrs={"type": "date"}
+                attrs={"type": "date", "class": "form-control"}
             ),
         }
